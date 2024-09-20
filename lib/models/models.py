420b5bdc46de94a66093629f54052571f488ae4c
Elviscryptic1981 <Elviskimani99@gmail.com>
2024-09-20 10:22:15 +0300

    updateModels

--- lib/models/models.py
+++ lib/models/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import SessionLocal
 
 DATABASE_URL = "sqlite:///./finance.db"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = SessionLocal(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
